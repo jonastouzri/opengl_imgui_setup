7290255a4e3da88b7a5641517935e37d2c1a7805
jonastouzri <85307094+jonastouzri@users.noreply.github.com>
2021-08-08 11:48:28 +0200

    simple imgui window

--- imgui_setup/Main.cpp
+++ imgui_setup/Main.cpp
@@ -10,7 +10,10 @@
 #include <glfw/glfw3.h>
 
 
-
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
 
 
 
@@ -19,32 +22,89 @@
 int main() {
 
 
-	//GLFWwindow* window = glfwCreateWindow(600, 600, "Test",NULL,NULL);
-	//glfwMakeContextCurrent(window);
-	//if(glfwInit())
-
-
-
 	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	//glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-	GLFWwindow* window = glfwCreateWindow(640, 480, "My Title", NULL, NULL);
-	if (window)
-		std::cout << "OK" << std::endl;
-	else
-		std::cout << "FAIL" << std::endl;
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 
+	if (window == NULL){
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
 
 	glfwMakeContextCurrent(window);
 
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+	
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);	// ??
 
 
 	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	ImGui::StyleColorsDark();
 
-	//ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 330");
 
-	//Imgui_ImplGlfw_InitForOpenGl(window, true);
+
+
+
+	while (!glfwWindowShouldClose(window)){
+
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
+		ImGui::Begin("window");
+		ImGui::Text("hello");
+		ImGui::End();
+
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		
+
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+
+
+
+	}
+
+
+
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	glfwTerminate();
+
+
+
+
+
+
+	//IMGUI_CHECKVERSION();
+	//ImGui::CreateContext();
+	//ImGuiIO& io = ImGui::GetIO();
+
+
+
 
 	//(void)io;
 
