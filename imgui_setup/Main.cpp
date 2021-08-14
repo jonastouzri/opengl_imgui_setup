cac9ebd8ca10b5cd9e65d71724a25019c597a4a7
jonastouzri <85307094+jonastouzri@users.noreply.github.com>
2021-08-14 23:37:17 +0200

    ok

--- imgui_setup/Main.cpp
+++ imgui_setup/Main.cpp
@@ -12,9 +12,6 @@
 #include <implot.h>
 
 
-
-
-
 #include <iostream>
 #include <glad/glad.h>
 #include <glfw/glfw3.h>
@@ -63,9 +60,43 @@ int main() {
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
 	ImGui::StyleColorsDark();
 
+	//ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+
 
 	ImGui_ImplGlfw_InitForOpenGL(window, true);
 	ImGui_ImplOpenGL3_Init("#version 330");
+
+	//-----------------------------------------------------------
+
+	//	// Start the Dear ImGui frame
+	//ImGui_ImplOpenGL3_NewFrame();
+	//ImGui_ImplGlfw_NewFrame();
+	//ImGui::NewFrame();
+
+	//// Create the docking environment
+	ImGuiWindowFlags windowFlags = ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoTitleBar |
+		ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
+		ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus |
+		ImGuiWindowFlags_NoBackground;
+
+	//ImGuiViewport* viewport = ImGui::GetMainViewport();
+	//ImGui::SetNextWindowPos(viewport->Pos);
+	//ImGui::SetNextWindowSize(viewport->Size);
+	//ImGui::SetNextWindowViewport(viewport->ID);
+
+	//ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+	//ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+	//ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+	//ImGui::Begin("InvisibleWindow", nullptr, windowFlags);
+	//ImGui::PopStyleVar(3);
+
+	//ImGuiID dockSpaceId = ImGui::GetID("InvisibleWindowDockSpace");
+
+	//ImGui::DockSpace(dockSpaceId, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_PassthruCentralNode);
+	//ImGui::End();
+
+	//-----------------------------------------------------------
+	//-----------------------------------------------------------
 
 
 
